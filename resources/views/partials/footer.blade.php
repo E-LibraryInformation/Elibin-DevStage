472a3aea5e3be16c871672ce0534b3dad827a9b8
putusekar <ayusekarr8@gmail.com>
2023-09-21 12:58:40 +0700

    create footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        /* Style untuk footer */
+        footer {
+            background-color: #333;
+            color: #fff;
+            padding: 20px;
+            text-align: center;
+        }
+
+        /* Style untuk teks besar "elibin" */
+        .footer-title {
+            font-size: 24px;
+            margin-bottom: 10px;
+        }
+
+        /* Style untuk garis bawah "elibin" */
+        .footer-title::after {
+            content: '';
+            display: block;
+            width: 80px;
+            height: 2px;
+            background-color: #fff;
+            margin: 0 auto;
+        }
+
+        /* Style untuk bagian "Information" */
+        .information {
+            font-size: 18px;
+            margin-top: 20px;
+        }
+
+        /* Style untuk bagian "Social Media" */
+        .social-media {
+            margin-top: 20px;
+        }
+
+        /* Style untuk ikon media sosial */
+        .social-media a {
+            text-decoration: none;
+            color: #fff;
+            margin: 0 10px;
+            font-size: 20px;
+        }
+    </style>
+</head>
+<body>
+    <footer>
+        <div class="footer-title">elibin</div>
+        <div class="information">Information</div>
+        <div class="social-media">
+            <a href="#">Facebook</a>
+            <a href="#">Twitter</a>
+            <a href="#">Instagram</a>
+        </div>
+    </footer>
+</body>
+</html>
