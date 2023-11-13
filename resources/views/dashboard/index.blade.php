dd7511d24c28b95eec2db5e2157baf7e44567434
Chilliope <chilliope48@gmail.com>
2023-11-13 23:49:23 +0800

    idk

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -4,12 +4,12 @@
     <div class="flex flex-row text-white gap-1 items-center p-2">
         <a href="/" class="text-sky-400">Dashboard</a>/
     </div>
-    <div class="bg-slate-800 w-full h-max border rounded-md shadow-md text-slate-300">
+    <div class="bg-slate-400 dark:bg-slate-800 w-full h-max border rounded-md shadow-md text-black dark:text-slate-300">
         <div class="p-4">
             <span class="text-xl">Elibin</span>
         </div>
         <div class="p-4">
-            <div class="bg-slate-700 border border-white w-full h-max rounded-lg">
+            <div class="bg-slate-500 dark:bg-slate-700 border border-white w-full h-max rounded-lg">
                 <div class="w-full h-max flex flex-col">
                     <div class="text-sm text-center mt-3">
                         Buku Terpopuler
@@ -17,12 +17,12 @@
                     <div>
                         <div class="p-4 flex justify-center flex-wrap gap-6">
                             @foreach($books as $book)
-                            <a href="/book/{{ $book->id }}" class="bg-slate-800 border border-white rounded-lg w-2/5 h-max overflow-hidden md:w-48 md:h-max hover:-translate-x-2 hover:-translate-y-2 hover:shadow-lg hover:shadow-purple-500 hover:duration-200">
+                            <a href="/book/{{ $book->id }}" class="bg-white dark:bg-slate-800 border border-white rounded-lg w-2/5 h-max overflow-hidden md:w-48 md:h-max hover:-translate-x-2 hover:-translate-y-2 hover:shadow-lg hover:shadow-purple-500 hover:duration-200">
                                 <div>
                                     <img src="{{ $book->gambar }}" alt="{{ $book->judul }}" class="w-48">
                                 </div>
-                                <div class="p-2 text-sm text-slate-300 flex flex-col">
-                                    <div class="text-slate-400 text-xs">
+                                <div class="p-2 text-sm text-black dark:text-slate-300 flex flex-col">
+                                    <div class="text-black dark:text-slate-400 text-xs">
                                         ID: {{ $book->id }}
                                     </div>
                                     <div class="font-bold w-full text-xs">
@@ -32,13 +32,13 @@
                                         <p class="text-xs">{{ substr($book->sinopsis, 0, 6) . '...' }}</p>
                                     </div>
                                     <div class="text-xs">
-                                        Penulis: <span class="text-slate-400">{{ substr($book->penulis, 0, 6) . '...' }}</span>
+                                        Penulis: <span class="text-black dark:text-slate-400">{{ substr($book->penulis, 0, 6) . '...' }}</span>
                                     </div>
                                     <div class="text-xs">
-                                        Rak: <span class="text-sky-400">{{ $book->rak }}</span>
+                                        Rak: <span class="text-sky-700 dark:text-sky-400">{{ $book->rak }}</span>
                                     </div>
                                     <div class="text-xs">
-                                        Stok: <span class="text-slate-400">{{ $book->stok }}</span>
+                                        Stok: <span class="text-black dark:text-slate-400">{{ $book->stok }}</span>
                                     </div>
                                     <div class="text-xs">
                                         Status : <span class="font-semibold {{ ($book->status === 'Available') ? 'text-lime-400' : 'text-red-400' }}">{{ $book->status }}</span>
@@ -56,24 +56,24 @@
                     </div>
                     <div class="md:p-4 flex flex-wrap justify-center gap-4">
                         @foreach ($writers as $writer)
-                        <a href="/profile/{{ $writer->id }}" class="bg-slate-800 border border-white rounded-lg w-2/5 h-max overflow-hidden md:w-48 md:h-max hover:-translate-x-2 hover:-translate-y-2 hover:shadow-lg hover:shadow-purple-500 hover:duration-200">
+                        <a href="/profile/{{ $writer->id }}" class="bg-white dark:bg-slate-800 border border-white rounded-lg w-2/5 h-max overflow-hidden md:w-48 md:h-max hover:-translate-x-2 hover:-translate-y-2 hover:shadow-lg hover:shadow-purple-500 hover:duration-200">
                             <div>
                                 <img src="{{ $writer->gambar }}" alt="{{ $writer->fullname }}" class="w-48">
                             </div>
                             <div class="p-2 text-sm text-slate-300 flex flex-col">
-                                <div class="text-slate-400 text-xs">
+                                <div class="text-black dark:text-slate-400 text-xs">
                                     ID: {{ $writer->id }}
                                 </div>
-                                <div class="font-bold w-full text-xs">
+                                <div class="font-bold w-full text-xs text-black dark:text-slate-400">
                                     {{  $writer->fullname }}
                                 </div>
                                 <div>
-                                    <p class="text-xs">{{ $writer->username }}</p>
+                                    <p class="text-xs text-black dark:text-slate-400">{{ $writer->username }}</p>
                                 </div>
                                 <div class="text-xs">
                                     <span class="text-red-400 uppercase font-bold">{{ $writer->role }}</span>
                                 </div>
-                                <div class="text-xs flex flex-row gap-2">
+                                <div class="text-xs flex flex-row gap-2 text-black dark:text-slate-400">
                                     <span>Followers : {{ $followerCounts[$writer->id] }}</span>|
                                     <p>Following: {{ $followingCounts[$writer->id] }}</p>
                                 </div>
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     @vite('resources/css/app.css')
 </head>
-<body class="bg-slate-700">
+<body class="bg-slate-300 text-black dark:bg-slate-700 dark:text-slate-300">
     @include('partials.navbar')
     <div class="w-full md:p-8 flex flex-col justify-center">
         @yield('container')
